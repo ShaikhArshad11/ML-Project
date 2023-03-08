cae1cbdd278added239dfa3c6fc680a6f2a4c9e4
Arshad Shaikh <shaikh121arshad@gmail.com>
2023-03-08 12:48:57 +0530

    commit

--- housing/entity/config_entity.py
+++ housing/entity/config_entity.py
@@ -20,3 +20,5 @@ ModelEvaluationConfig = namedtuple("ModelEvaluationConfig", ["model_evaluation_f
                                                              "time_stamp"])
 
 ModelPusherConfig = namedtuple("ModelPusherConfig", ["export_dir_path"])
+
+TrainingPipelineConfig = namedtuple("TrainingPipelineConfig", ["artifact_dir"])
